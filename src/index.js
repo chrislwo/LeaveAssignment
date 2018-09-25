c0b7b8c1af0a19ed41977772ffa970c238512d70
chrislwo <43380573+chrislwo@users.noreply.github.com>
2018-09-25 13:36:33 +0800

    step2: ajax call from other URI in lifecycle componentdidmount

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TodoApp from './step1/App';
+import TodoApp from './step2/App';
 import registerServiceWorker from './registerServiceWorker';
 
+//TODO 
+//https://www.robinwieruch.de/react-fetching-data/
 ReactDOM.render(<TodoApp />, document.getElementById('root'));
 registerServiceWorker();
